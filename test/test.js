89e3899aa7069c149c236b7146574e4883e6754a
Pete Schallot <schallot.code@gmail.com>
2019-12-08 09:18:37 -0500

    Create log table.

--- test/test.js
+++ test/test.js
@@ -2,17 +2,23 @@ const describe = require('mocha').describe;
 const it = require('mocha').it;
 const log4js = require('log4js');
 const assert = require('assert');
+const sqlite = require('sqlite3').verbose();
+const fs = require('fs');
 
 describe('log4js', function() {
   describe('log', function() {
-    it('should log to sqlite db', function() {
-
+    it('should log to sqlite db', async function(done) {
+      const dbFile = './test/test.sqlite';
+      if(fs.existsSync(dbFile)){
+        fs.unlinkSync(dbFile);
+      }
+      const logTable = 'logTest';
       log4js.configure({
         appenders: {
           database: {
             type: "lib/log4js-sqlite3",
-            filename: './test.sqlite',
-            table: "log",
+            filename: dbFile,
+            table: logTable,
             useNullAsDefault: true,
             additionalFields: [
               {name: 'customColumn1', value: 'custom value 1', type: 'TEXT' },
@@ -33,11 +39,43 @@ describe('log4js', function() {
       const defaultLog = log4js.getLogger();
       const anotherLog = log4js.getLogger('ANOTHER_CATEGORY');
 
+      await new Promise(((resolve, reject) => {
+        setTimeout(resolve, 3000);
+      }));
+
       defaultLog.info('first log entry');
       defaultLog.trace('this log entry is too low of a level to reach the database');
       anotherLog.error('this error message should show up with category set to ANOTHER_CATEGORY');
 
-      assert.fail('test not yet completely implemented');
+      const db = new sqlite.Database(dbFile);
+      await new Promise((resolve, reject) => {
+        try{
+          log4js.shutdown((err) => {
+            if(err){
+              reject(err);
+            }
+            else{
+              resolve();
+            }
+          })
+        }
+        catch (e) {
+          reject(e);
+        }
+      });
+
+      const resultRows = await new Promise((resolve, reject) => {
+        const sql = ``;
+        const params = [];
+        db.all(sql,params,(err, rows ) => {
+          resolve(rows);
+        });
+      });
+
+      assert.equal(resultRows.length, 3, 'Unexpected number of log rows were written.');
+
+      db.close();
+      done();
     });
   });
 });
